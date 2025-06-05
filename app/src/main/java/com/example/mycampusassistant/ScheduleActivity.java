45e26fb04abee5a47cd81e877b99b2781c095233
Davie chiwata <daviechiwata@gmail.com>
2025-06-05 15:45:39 +0200

    added a class schedule

---
+++ app/src/main/java/com/example/mycampusassistant/ScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.example.mycampusassistant;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.class_schedule);  // Make sure this matches your XML layout file name
+    }
+}
